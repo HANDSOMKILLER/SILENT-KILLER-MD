6b2c99a8ca0852a91544a474db5a0b1205032e27
🅂𝙞𝙡𝙚𝙣𝙩 𝙠𝙞𝙡𝙡𝙚𝙧 <166059547+HANDSOMKILLER@users.noreply.github.com>
2024-10-18 16:01:43 +0500

    Update converter.js

--- plugins/converter.js
+++ plugins/converter.js
@@ -8,8 +8,8 @@ const {cmd , commands} = require('../command');
 
 
 cmd({
-    pattern: "photo",
-    desc: "sticker to pjoto",
+    pattern: "take",
+    desc: "sticker pack Change",
     category: "main",
     react: "⛱️",
     filename: __filename
@@ -17,8 +17,43 @@ cmd({
 
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-   if (!message.reply_message?.sticker) return await message.reply("_Reply to a sticker_");
-   if (message.reply_message.isAnimatedSticker) return await message.reply("_Reply to a non-animated sticker message_");
-   let buffer = await webpToPng(await message.reply_message.downloadAndSave());
-   return await message.send(buffer, {}, "image");
-});
+
+   let data;
+   if (!message.reply_message || (!message.reply_message.sticker && !message.reply_message.audio)) return await message.reply("_Reply to a sticker or audio_");
+   if (message.reply_message.sticker) {
+   const stickerPackName = (match || config.STICKER_PACKNAME).split(";");
+   await message.send(await message.reply_message.download(), { packname: stickerPackName[0], author: stickerPackName[1] }, "sticker");
+   } else if (message.reply_message.audio) {
+   const buff = await message.reply_message.download();
+   const audioBuffer = Buffer.from(buff);
+   const audioResult = await toAudio(audioBuffer, 'mp4');
+   if (match) data = match.split(";");
+   data = config.AUDIO_DATA.split(";");
+   await message.reply(await AddMp3Meta(audioResult, await getBuffer(data[2]), { title: data[0], body: data[1] }), { mimetype: "audio/mp4" }, "audio");
+}});
+
+
+cmd({
+    pattern: "take",
+    desc: "sticker pack Change",
+    category: "main",
+    react: "⛱️",
+    filename: __filename
+},
+
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+   let data;
+   if (!message.reply_message || (!message.reply_message.sticker && !message.reply_message.audio)) return await message.reply("_Reply to a sticker or audio_");
+   if (message.reply_message.sticker) {
+   const stickerPackName = (match || config.STICKER_PACKNAME).split(";");
+   await message.send(await message.reply_message.download(), { packname: stickerPackName[0], author: stickerPackName[1] }, "sticker");
+   } else if (message.reply_message.audio) {
+   const buff = await message.reply_message.download();
+   const audioBuffer = Buffer.from(buff);
+   const audioResult = await toAudio(audioBuffer, 'mp4');
+   if (match) data = match.split(";");
+   data = config.AUDIO_DATA.split(";");
+   await message.reply(await AddMp3Meta(audioResult, await getBuffer(data[2]), { title: data[0], body: data[1] }), { mimetype: "audio/mp4" }, "audio");
+}});
