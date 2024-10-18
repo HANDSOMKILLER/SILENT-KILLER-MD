3c2e8b26d95c1b99f588e6196fc0fd4903e148e2
🅂𝙞𝙡𝙚𝙣𝙩 𝙠𝙞𝙡𝙡𝙚𝙧 <166059547+HANDSOMKILLER@users.noreply.github.com>
2024-10-18 17:39:45 +0500

    Update gitstalk.js

--- plugins/gitstalk.js
+++ plugins/gitstalk.js
@@ -20,7 +20,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        let userInfo = `     👨‍💻*SILENT-SOBX-MD GITSTALK*👨‍💻
+        let userInfo = `     👨‍💻*SILENT-KILLER-MD GITSTALK*👨‍💻
         
 👤 *ᴜꜱᴇʀ ɴᴀᴍᴇ*: ${data.name || data.login}
 
@@ -38,7 +38,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
 🔭 *ᴘᴜʙʟɪᴄ ɢɪꜱᴛꜱ*: ${data.public_gists}
 
-*MADE ♥ BY SILENT-SOBX-MD*
+*MADE ♥ BY SILENT-KILLER-MD*
 `;
 
         await conn.sendMessage(from, { image: { url: data.avatar_url }, caption: userInfo }, { quoted: mek });
