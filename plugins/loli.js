0630d3d440df0d25b83f1941472eeee645448704
🅂𝙞𝙡𝙚𝙣𝙩 𝙠𝙞𝙡𝙡𝙚𝙧 <166059547+HANDSOMKILLER@users.noreply.github.com>
2024-10-19 09:08:43 +0500

    Update loli.js

--- plugins/loli.js
+++ plugins/loli.js
@@ -6,7 +6,7 @@ cmd({
     alias: ["lolii"],
     desc: "Fetch a random anime girl image.",
     category: "fun",
-    react: "🐱",
+    react: "😝",
     filename: __filename
 },
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
@@ -15,7 +15,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *SILENT-SOBX-MD RANDOM ANIME GIRL IMAGES* 👸\n\n\n *🧬©SILENT-SOBX-MD BY SILENTLOVER432*' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *SILENT-KILLER-MD RANDOM ANIME GIRL IMAGES* 👸\n\n\n *🧬©SILENT-KILLER-MD BY ❉্᭄͜͡ *🅐︎ᴡᴀɪs 🅙︎ᴀɴɪɪ* ❉্᭄͜͡' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`*Error Fetching Anime Girl image*: ${e.message}`);
