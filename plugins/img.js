9c8772344e54119f1089554d2896705f29cb0cc2
🅂𝙞𝙡𝙚𝙣𝙩 𝙠𝙞𝙡𝙡𝙚𝙧 <166059547+HANDSOMKILLER@users.noreply.github.com>
2024-10-19 09:03:18 +0500

    Update img.js

--- plugins/img.js
+++ plugins/img.js
@@ -43,7 +43,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
  *Enjoy these images! 👾*
 
-> 🌈*SILENT-SOBX-MD BOT PLUGINS*🏝️`
+> 🌈*SILENT-KILLER-MD BOT PLUGINS*🏝️`
 }, { quoted: mek });
 }
 
