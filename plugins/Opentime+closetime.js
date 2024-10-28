8a49bc91be9fca5e42528e9fa599f296c4ead8b2
🅂𝙞𝙡𝙚𝙣𝙩 𝙠𝙞𝙡𝙡𝙚𝙧 <166059547+HANDSOMKILLER@users.noreply.github.com>
2024-10-28 21:43:04 +0500

    Create Opentime+closetime.js

---
+++ plugins/Opentime+closetime.js
@@ -0,0 +1,109 @@
+const config = require('../config')
+const { cmd, commands } = require('../command')
+
+cmd({
+    pattern: "opentime",
+    react: "🔖",
+    desc: "To open group to a time",
+    category: "group",
+    use: '.opentime',
+    filename: __filename
+},
+async(conn, mek, m,{from, prefix, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{   
+if (!isGroup) return reply(ONLGROUP)
+if (!isAdmins) return reply(ADMIN)	
+  if (args[1] == 'second') {
+                    var timer = args[0] * `1000`
+                } else if (args[1] == 'minute') {
+                    var timer = args[0] * `60000`
+                } else if (args[1] == 'hour') {
+                    var timer = args[0] * `3600000`
+                } else if (args[1] == 'day') {
+                    var timer = args[0] * `86400000`
+                } else {
+                    return reply('*select:*\nsecond\nminute\nhour\n\n*example*\n10 second')
+                }
+                reply(`Open time ${q} starting from now`)
+                setTimeout(() => {
+                    var nomor = mek.participant
+                    const open = `*OPEN TIME* THE GROUP WAS OPENED BY SILENT-SOBX-MD TO APPROVED ADMIN\n NOW MEMBERS CAN SEND MESSAGES 🔓`
+                    conn.groupSettingUpdate(from, 'not_announcement')
+                    reply(open)
+                }, timer)
+await conn.sendMessage(from, { react: { text: `✅`, key: mek.key }}) 
+} catch (e) {
+reply('*Error !!*')
+l(e)
+}
+})
+
+cmd({
+    pattern: "closetime",
+    react: "🔖",
+    desc: "To close group to a time",
+    category: "group",
+    use: '.closstime',
+    filename: __filename
+},
+async(conn, mek, m,{from, prefix, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{   
+if (!isGroup) return reply(ONLGROUP)
+if (!isAdmins) return reply(ADMIN)	
+                if (args[1] == 'second') {
+                    var timer = args[0] * `1000`
+                } else if (args[1] == 'minute') {
+                    var timer = args[0] * `60000`
+                } else if (args[1] == 'hour') {
+                    var timer = args[0] * `3600000`
+                } else if (args[1] == 'day') {
+                    var timer = args[0] * `86400000`
+                } else {
+                    return reply('*select:*\nsecond\nminute\nhour\n\n*Example*\n10 second')
+                }
+                reply(`Close time ${q} starting from now`)
+                setTimeout(() => {
+                    var nomor = m.participant
+                    const close = `*CLOSE TIME* GROUP CLOSED BY SILENT-SOBX-MD AT APPROVED ADMIN\nNOW ONLY ADMIN CAN SEND MESSAGES 🔐`
+                    conn.groupSettingUpdate(from, 'announcement')
+                    reply(close)
+                }, timer)
+await conn.sendMessage(from, { react: { text: `✅`, key: mek.key }}) 
+} catch (e) {
+reply('*Error !!*')
+l(e)
+}
+})
+
+
+cmd({
+    pattern: "tagadmin",
+    alais:["tagadmins"],
+    react: "🙀",
+    desc: "Tags all the admins in the group.",
+    category: "group",
+    filename: __filename,
+},           
+async(conn, mek, m,{from, prefix, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+        // Check if the command is used in a group
+        if (!isGroup) return reply(`This command is only for groups.`);
+        if (!isAdmins) return reply(`This command is only for group admin.`);
+        
+        // Fetch all group admins
+        const admins = groupAdmins;
+        if (admins.length === 0) {
+            return reply('There are no admins in this group.');
+        }
+        // Create a message with all admin tags
+        let adminTagMessage = '*TAGGING ALL ADMINS IN THE GROUP 🔳:*\n\n';
+        for (let admin of admins) {
+            adminTagMessage += `@${admin.split('@')[0]}\n`;  // Mention each admin by their number
+        }
+        // Send the message and tag the admins
+        await conn.sendMessage(from, { text: adminTagMessage, mentions: admins }, { quoted: mek });
+    } catch (e) {
+        console.error('Error tagging admins:', e);
+        reply('you are not an admin.');
+    }
+})
